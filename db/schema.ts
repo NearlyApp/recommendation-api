6007732e372e88f570ab1549ab5165050a77bcee
Khai <nkphan@cegid.com>
2025-09-12 11:19:44 +0200

    added swagger + removed old postgres db

--- db/schema.ts
+++
@@ -1,9 +0,0 @@
-import { pgTable, uuid, vector } from "drizzle-orm/pg-core";
-
-export const embeddings = pgTable("embeddings", {
-  id: uuid("id").primaryKey().defaultRandom(),
-  postId: uuid("post_id").notNull(),
-  embeddings: vector("embeddings", {
-    dimensions: 1024,
-  }),
-});
